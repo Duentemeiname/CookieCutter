717cdbfe6a8ab0b132262d5208804bcee4ec54d8
Luca Dünte <luca.duente@home.duentetech.de>
2024-03-06 17:16:18 +0100

    Jeder Cheese erhält nun ein Herkunftsland

--- everycheese/cheeses/tests/factories.py
+++ everycheese/cheeses/tests/factories.py
@@ -9,6 +9,7 @@ class CheeseFactory(factory.django.DjangoModelFactory):
     name = factory.fuzzy.FuzzyText()
     slug = factory.LazyAttribute(
         lambda obj: slugify(obj.name))
+    country_of_origin = factory.Faker('country_code')
     description = factory.Faker(
         'paragraph', nb_sentences=3,
         variable_nb_sentences=True
